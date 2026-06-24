4f40fe65fca6e463e692842b60bcd9a8617dc4f3
ScotPutnam <scot.putnam@my.uwrf.edu>
2026-06-24 15:25:38 -0500

    changes to script

--- db/Scripts/Script-2.sql
+++ db/Scripts/Script-2.sql
@@ -6,25 +6,26 @@ DROP TABLE IF EXISTS users;
 
 
 CREATE TABLE IF NOT EXISTS users (
- user_id INTEGER PRIMARY KEY AUTOINCREMENT,
- username TEXT UNIQUE NOT NULL,
- password TEXT NOT NULL,
- role TEXT NOT NULL);
+	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	username TEXT UNIQUE NOT NULL,
+	password TEXT NOT NULL,
+	role TEXT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS expenses(
-   expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
-   amount REAL,
-   description TEXT,
-   expense_date TEXT,
-   user_id_fk INTEGER NOT NULL,
-   FOREIGN KEY (user_id_fk) REFERENCES users(user_id));
+	expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	amount REAL,
+	description TEXT,
+	expense_date TEXT,
+	user_id_fk INTEGER NOT NULL,
+	FOREIGN KEY (user_id_fk) REFERENCES users(user_id));
 
 CREATE TABLE IF NOT EXISTS approvals(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    expense_id_fk INTEGER,
-    status TEXT,
-    reviewer INTEGER,
-    comment TEXT,
-    review_date TEXT,
-    FOREIGN KEY (expense_id_fk) REFERENCES expenses(expense_id)
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	expense_id_fk INTEGER,
+	status TEXT,
+	reviewer INTEGER,
+	comment TEXT,
+	review_date TEXT,
+	FOREIGN KEY (reviewer) REFERENCES expenses(user_id_fk),
+	FOREIGN KEY (expense_id_fk) REFERENCES expenses(expense_id)
 );
