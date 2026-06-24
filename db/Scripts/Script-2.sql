c6c95d7dcfae895af9c06352720d208d95d178a1
ScotPutnam <scot.putnam@my.uwrf.edu>
2026-06-24 15:20:46 -0500

    updating

--- db/Scripts/Script-2.sql
+++ db/Scripts/Script-2.sql
@@ -1,29 +1,30 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS approvals;
 DROP TABLE IF EXISTS expenses;
 DROP TABLE IF EXISTS users;
 
+
 CREATE TABLE IF NOT EXISTS users (
-	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	username TEXT UNIQUE NOT NULL,
-	password TEXT NOT NULL,
-	role TEXT NOT NULL);
+ user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ username TEXT UNIQUE NOT NULL,
+ password TEXT NOT NULL,
+ role TEXT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS expenses(
-	expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	amount REAL,
-	description TEXT,
-	expense_date TEXT,
-	user_id_fk INTEGER NOT NULL,
-	FOREIGN KEY (user_id_fk) REFERENCES users(user_id));
+   expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   amount REAL,
+   description TEXT,
+   expense_date TEXT,
+   user_id_fk INTEGER NOT NULL,
+   FOREIGN KEY (user_id_fk) REFERENCES users(user_id));
 
 CREATE TABLE IF NOT EXISTS approvals(
-	approval_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	expense_id_fk INTEGER NOT NULL,
-	FOREIGN KEY (expense_id_fk) REFERENCES expenses(expense_id),
-	status TEXT,
-	reviewer_id INTEGER,
-	FOREIGN KEY (reviewer_id) REFERENCES expenses(user_id_fk),
-	comment TEXT,
-	review_date TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    expense_id_fk INTEGER,
+    status TEXT,
+    reviewer INTEGER,
+    comment TEXT,
+    review_date TEXT,
+    FOREIGN KEY (expense_id_fk) REFERENCES expenses(expense_id)
 );
